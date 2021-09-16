71df2df28fdf37f5e15d2cfb207558d8bb50a1a3
Mountain Scott <mountainscott@yahoo.com>
2021-09-16 15:23:27 -0700

    added persistance to data and corrected errors

--- routes/genres.js
+++ routes/genres.js
@@ -1,53 +1,95 @@
 const express = require('express');
+const Joi = require('joi');
+const mongoose = require('mongoose');
 const router = express.Router();
 
-const genres = [
-  { id: 1, name: 'Action' },  
-  { id: 2, name: 'Horror' },  
-  { id: 3, name: 'Romance' },  
-];
+
+// const genres = [
+//   { id: 1, name: 'Action' },  
+//   { id: 2, name: 'Horror' },  
+//   { id: 3, name: 'Romance' },  
+// ];
+
+
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+}));
+
+
 
 router.get('/', (req, res) => {
-  res.send(genres);
+  Genre.find().sort({ name: 1 })
+    .then(genres => {
+      res.send(genres);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving genres."
+      });
+    });
+  // res.send(genres);
 });
+
 
 router.post('/', (req, res) => {
   const { error } = validateGenre(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = {
-    id: genres.length + 1,
+  let genre = new Genre({
     name: req.body.name
-  };
-  genres.push(genre);
-  res.send(genre);
+  });
+  genre.save().then(() => {
+    res.send(genre)
+  }).catch(() => {
+    res.status(400).send('Invalid genre');
+  })
 });
+
 
 router.put('/:id', (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
-  const { error } = validateGenre(req.body); 
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).send('Invalid ID');
+  }
+  const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  
-  genre.name = req.body.name; 
-  res.send(genre);
-});
+
+  Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true })
+    .then(genre => {
+      if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+      res.send(genre);
+    }).catch(() => {
+      res.status(400).send('Invalid ID');
+    })
+})
+
+
 
 router.delete('/:id', (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-
-  res.send(genre);
+  
+  Genre.findByIdAndRemove(req.params.id)
+  .then(genre => {  res.send(genre); })
+  .catch(() => {
+    res.status(404).send('Invalid ID');
+  })
 });
 
 router.get('/:id', (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  res.send(genre);
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).send('Invalid ID');
+  }
+  Genre.findById(req.params.id)
+    .then(genre => {
+      if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+      res.send(genre);
+    })
+    .catch(() => {
+      res.status(400).send('Invalid ID');
+    })
 });
 
 function validateGenre(genre) {
